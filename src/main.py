3f20d6f91abb27b75389cfdd84f19e37e566c75a
Milan Bontridder <119121532+mbontrid@users.noreply.github.com>
2026-03-16 14:05:03 +0100

    quick and dirty tensor load

--- src/main.py
+++ src/main.py
@@ -1,5 +1,6 @@
 from utils.formater import LoaderToNumpy
 from utils.args import Args
+from models.Elman_SRN.torch_Elman_SRN import ElmanSRN
 
 
 def main():
@@ -8,8 +9,15 @@ def main():
     loader = LoaderToNumpy(args.file_path, args.format)
 
     data = loader.get()
+    data = data.flatten()
     print(data)
-    print(type(data))
+    print(data.dtype)
+    print(data.shape)
+    print(type(data[0]))
+
+    srn = ElmanSRN()
+
+    srn.load_numpy(data)
 
 
 if __name__ == "__main__":
--- src/models/Elman_SRN/torch_Elman_SRN.py
+++ src/models/Elman_SRN/torch_Elman_SRN.py
@@ -1,8 +1,14 @@
 import torch
 import torch.nn as nn
 from torch.utils.data import DataLoader
+import numpy as np
 
 
 class ElmanSRN(nn.Module):
     def __init__(self, sizes: list[int] = [8, 20, 8]):
         pass
+
+    def load_numpy(self, ndarray: np.ndarray):
+        ndarray = np.array([ord(c) if isinstance(c, str) else c for c in ndarray])
+        training_data = torch.from_numpy(ndarray)
+        print(training_data)
